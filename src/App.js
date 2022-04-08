870cded24be04c3d3713f6cc27996d1b6c1ce97a
Jeswin-Neo <jeswin@neoito.com>
2022-04-08 23:33:03 +0530

    migrated to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import * as d3 from "d3";
+import graph from "./data/graph";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const width = 1800,
+    height = 900;
+  useEffect(() => {
+    const svg = d3
+      .select("div")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    graph.content.forEach(function (d) {
+      d.source = graph.nodes[d.source];
+      d.target = graph.nodes[d.target];
+    });
+
+    // console.log(graph);
+
+    const link = svg
+      .append("g")
+      .attr("class", "link")
+      .selectAll("line")
+      .data(graph.content)
+      .enter()
+      .append("line")
+      .attr("fill", "#DCDCDC")
+      .attr("x1", function (d) {
+        return d.source.x + 70;
+      })
+      .attr("y1", function (d) {
+        return d.source.y;
+      })
+      .attr("x2", function (d) {
+        return d.target.x + 50;
+      })
+      .attr("y2", function (d) {
+        return d.target.y + 10;
+      });
+
+    svg
+      .append("g")
+      .attr("class", "node")
+      .selectAll("rect")
+      .data(graph.nodes)
+      .enter()
+      .append("rect")
+      .attr("class", (d, i) => "rect" + i)
+      .attr("width", "230px")
+      .attr("height", "130px")
+      .style("fill", "no")
+      .style("rx", 6)
+      .attr("x", function (d) {
+        return d.x;
+      })
+      .attr("y", function (d) {
+        return d.y;
+      })
+      .call(d3.drag().on("drag", dragged));
+
+    const text = svg
+      .selectAll("text")
+      .data(graph.content)
+      .enter()
+      .append("text")
+      .attr("x", (d, i) => d.x + 40)
+      .attr("y", (d, i) => d.y + 20)
+      .style("font-size", "11px")
+      .style("font-family", "Helvetica")
+      .style("font-weight", 800)
+      .style("letter-spacing", "1px")
+      .text((d) => d.text);
+
+    const icons = svg
+      .selectAll("icons")
+      .data(graph.content)
+      .enter()
+      .append("a")
+      .append("image")
+      .attr("class", "icons")
+      .attr("x", (d, i) => d.x + 2)
+      .attr("y", (d, i) => d.y - 2)
+      .attr("width", 35)
+      .attr("height", 35)
+      .attr("xlink:href", (d) => d.image);
+
+    const googleImg = svg
+      .selectAll("googleImg")
+      .data(graph.content)
+      .enter()
+      .append("image")
+      .attr("x", (d, i) => d.x + 85)
+      .attr("y", (d, i) => d.y + 40)
+      .attr("class", "googleImg")
+      .attr("width", 55)
+      .attr("height", 55)
+      .attr("href", (d) => d.google);
+
+    function dragged(event, d) {
+      d.x = event.x;
+      d.y = event.y;
+      d3.select(this).attr("x", d.x).attr("y", d.y);
+      text
+        .filter((t) => t.source === d)
+        .attr("x", d.x + 40)
+        .attr("y", d.y + 20);
+      icons
+        .filter((t) => t.source === d)
+        .attr("x", d.x + 2)
+        .attr("y", d.y - 2);
+      googleImg
+        .filter((t) => t.source === d)
+        .attr("x", d.x + 85)
+        .attr("y", d.y + 40);
+
+      link
+        .filter(function (l) {
+          return l.source === d;
+        })
+        .attr("x1", d.x + 70)
+        .attr("y1", d.y);
+      link
+        .filter(function (l) {
+          return l.target === d;
+        })
+        .attr("x2", d.x + 70)
+        .attr("y2", d.y + 10);
+    }
+  }, []);
+  return <div className="app"></div>;
 }
 
 export default App;
---
+++ src/data/graph.js
@@ -0,0 +1,65 @@
+import GoogleImg from "../images/google.png";
+import BlackIcon from "../images/black.png";
+import GreenIcon from "../images/green.png";
+
+export default {
+  nodes: [
+    { x: 400, y: 175 },
+    { x: 200, y: 500 },
+    { x: 500, y: 500 },
+    { x: 800, y: 500 },
+    { x: 1100, y: 500 },
+    { x: 1400, y: 500 },
+  ],
+  content: [
+    {
+      image: BlackIcon,
+      google: GoogleImg,
+      text: "Google",
+      x: 400,
+      y: 175,
+      source: 0,
+      target: 0,
+    },
+    {
+      image: GreenIcon,
+      text: "New York-Sites",
+      x: 200,
+      y: 500,
+      source: 1,
+      target: 0,
+    },
+    {
+      image: GreenIcon,
+      text: "New Jersey-Sites",
+      x: 500,
+      y: 500,
+      source: 2,
+      target: 0,
+    },
+    {
+      image: GreenIcon,
+      text: "Vermont-Sites",
+      x: 800,
+      y: 500,
+      source: 3,
+      target: 0,
+    },
+    {
+      image: GreenIcon,
+      text: "Massachusetts-Sites",
+      x: 1100,
+      y: 500,
+      source: 4,
+      target: 0,
+    },
+    {
+      image: GreenIcon,
+      text: "California-Sites",
+      x: 1400,
+      y: 500,
+      source: 5,
+      target: 0,
+    },
+  ],
+};
